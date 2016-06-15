9e22543bfda901e600fae1516175ef2325ad90e5
jerodestapa <jerodestapa@gmail.com>
2016-06-15 18:57:57 -0500

    Added django-forms-bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 # blog - urls.py
 
 from django.conf.urls import url
-from blog import views
+import views
 
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
+    url(r'^(?P<post_id>\w)$', views.post, name='post'),
+    url(r'^add_post/', views.add_post, name='add_post'),
 ]
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Local apps
+    # Third Party Apps
+    'django_forms_bootstrap',
+
+    # Local Apps
     'blog',
 ]
 
